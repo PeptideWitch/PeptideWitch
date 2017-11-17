d1998f74a02319ca72b08f8ca299390058d5ab78
David Handler <david.handler@students.mq.edu.au>
2017-11-17 16:18:47 +1100

    Update setup.py
    Removed packages from setup()

--- setup.py
+++ setup.py
@@ -3,13 +3,6 @@ from distutils.core import setup
 setup(
     name='PepWitch',
     version='1.2',
-    packages=['build.PepWitch1.1.setuptools-27.2.0-py3.6.egg.setuptools',
-              'build.PepWitch1.1.setuptools-27.2.0-py3.6.egg.setuptools.extern',
-              'build.PepWitch1.1.setuptools-27.2.0-py3.6.egg.setuptools.command',
-              'build.PepWitch1.1.setuptools-27.2.0-py3.6.egg.pkg_resources',
-              'build.PepWitch1.1.setuptools-27.2.0-py3.6.egg.pkg_resources.extern',
-              'build.PepWitch1.1.setuptools-27.2.0-py3.6.egg.pkg_resources._vendor',
-              'build.PepWitch1.1.setuptools-27.2.0-py3.6.egg.pkg_resources._vendor.packaging'],
     url='',
     license='Free to use, edit and distribute on condition of a citation for any research that employs the use of this code or any of its derivatives (in whole or in part)',
     author='David Handler',
