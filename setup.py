1b5a9e39f1d2f39d404bbba6f74975178808cb11
David Handler <david.handler@students.mq.edu.au>
2017-11-20 15:06:33 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='PepWitch',
     version='1.2',
-    packages=[numpy==1.13.3,matplotlib==2.1.0,scipy==1.0.0,matplotlib-venn==0.11.5,statsmodels==0.8.0],
+    packages=[cython=0.27.3,conda=4.3.16,numpy==1.13.3,matplotlib==2.1.0,scipy==1.0.0,matplotlib-venn==0.11.5,statsmodels==0.8.0],
     url='',
     license='Free to use, edit and distribute on condition of a citation for any research that employs the use of this code or any of its derivatives (in whole or in part)',
     author='David Handler',
@@ -11,4 +11,4 @@ setup(
     description='NSAF and high stringency protein ID calculations based on adjusted Q value analysis.'
 )
 
-install_requires=[numpy==1.13.3,matplotlib==2.1.0,scipy==1.0.0,matplotlib-venn==0.11.5,statsmodels==0.8.0],
+install_requires=[cython=0.27.3,conda=4.3.16,numpy==1.13.3,matplotlib==2.1.0,scipy==1.0.0,matplotlib-venn==0.11.5,statsmodels==0.8.0],
