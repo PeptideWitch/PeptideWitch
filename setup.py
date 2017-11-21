54251ac49812ca4abbc08ac32d5f641cee10bbef
David Handler <david.handler@students.mq.edu.au>
2017-11-21 22:50:25 +1100

    Update setup.py
    Working list of modules added

--- setup.py
+++ setup.py
@@ -3,7 +3,20 @@ from distutils.core import setup
 setup(
     name='PepWitch',
     version='1.2',
-    packages=[cython=0.27.3,conda=4.3.16,numpy==1.13.3,matplotlib==2.1.0,scipy==1.0.0,matplotlib-venn==0.11.5,statsmodels==0.8.0],
+    packages=[cycler==0.10.0
+    matplotlib==2.1.0
+    matplotlib-venn==0.11.5
+    numpy==1.13.3
+    olefile==0.44
+    pandas==0.21.0 
+    patsy==0.4.1
+    Pillow==4.3.0
+    pyparsing==2.2.0
+    python-dateutil==2.6.1
+    pytz==2017.3
+    scipy==1.0.0
+    six==1.11.0
+    statsmodels==0.8.0],
     url='',
     license='Free to use, edit and distribute on condition of a citation for any research that employs the use of this code or any of its derivatives (in whole or in part)',
     author='David Handler',
@@ -11,4 +24,17 @@ setup(
     description='NSAF and high stringency protein ID calculations based on adjusted Q value analysis.'
 )
 
-install_requires=[cython=0.27.3,conda=4.3.16,numpy==1.13.3,matplotlib==2.1.0,scipy==1.0.0,matplotlib-venn==0.11.5,statsmodels==0.8.0],
+install_requires=[cycler==0.10.0
+    matplotlib==2.1.0
+    matplotlib-venn==0.11.5
+    numpy==1.13.3
+    olefile==0.44
+    pandas==0.21.0
+    patsy==0.4.1
+    Pillow==4.3.0
+    pyparsing==2.2.0
+    python-dateutil==2.6.1
+    pytz==2017.3
+    scipy==1.0.0
+    six==1.11.0
+    statsmodels==0.8.0],
